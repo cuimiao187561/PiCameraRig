e83672cacefc60ee2072dedfdce420ffde807913
Chris Hunt <mail@thisisthechris.co.uk>
2016-01-09 01:37:45 +0000

    Adds camera node setup script

---
+++ camera-node/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo Starting....
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+cat system-requirements.txt | xargs sudo apt-get install -y
+
+sudo pip install -r python-requirements.txt
+
+sudo cp camera.conf /etc/supervisor/conf.d/
+
+sudo supervisorctl reload
+
+sudo mkdir /media/usb
+
+sudo reboot
