70770ab058da0d86e96e024c209b414c34378d60
Chris Hunt <mail@thisisthechris.co.uk>
2016-01-09 01:36:30 +0000

    Adds Camera code

---
+++ camera-node/camera.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import threading
+import time
+import picamera
+
+
+class cameraThread(threading.Thread):
+    # setup the thread object
+    # def __init__(self, id, brokerURL, brokerPort, settings):
+    def __init__(self, filePath, settings):
+        threading.Thread.__init__(self)
+
+        # settings
+        self.filePath = filePath
+
+        # set up camera
+        self.camera = picamera.PiCamera()
+        self.camera.start_preview()
+
+        # load settings
+        self.camera.hflip = settings["hflip"]
+        self.camera.vflip = settings["vflip"]
+
+    def capture(self):
+        now = time.time()
+        self.camera.capture(self.filePath + str(now) + '.jpg')
