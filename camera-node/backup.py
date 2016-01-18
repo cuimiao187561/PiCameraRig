e5164a4aacf1f4155a1c264f97adefa64acee6ab
Chris Hunt <mail@thisisthechris.co.uk>
2016-01-18 00:32:51 +0000

    Adds Backup Script

---
+++ camera-node/backup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import time
+import threading
+import os
+import json
+
+class backupThread(threading.Thread):
+    def __init__(self, backupMountScript, localImageFolder, backupDirectory):
+        threading.Thread.__init__(self)
+
+        self.localImageFolder = localImageFolder
+        self.backupDirectory = backupDirectory
+
+        print "mounting nas"
+        os.system(backupMountScript)
+
+    def start(self):
+        #time.sleep(30)
+        while True:
+            #print "backing up"
+            os.system("sudo rsync -a "+self.localImageFolder+" "+self.backupDirectory)
+            time.sleep(10)
+
+
+settings = None
+
+time.sleep(60)
+
+with open('/media/usb/settings.json') as data_file:
+    settings = json.load(data_file)
+
+b = backupThread(
+    settings["backupMountScript"],
+    settings["localImageFolder"],
+    settings["backupDirectory"]
+)
+
+b.start()
--- camera-node/setup.sh
+++ camera-node/setup.sh
@@ -10,9 +10,11 @@ cat system-requirements.txt | xargs sudo apt-get install -y
 sudo pip install -r python-requirements.txt
 
 sudo cp camera.conf /etc/supervisor/conf.d/
+sudo cp backup.conf /etc/supervisor/conf.d/
 
 sudo supervisorctl reload
 
 sudo mkdir /media/usb
+sudo mkdir /media/nas
 
 sudo reboot
